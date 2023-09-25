b80aa5abce5351d27d1f19f38a1431839e079338
mizan-chowdhury <mizan1034@gmail.com>
2023-09-25 17:20:12 +0600

    responsived the navbar and some edited on banner section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,48 +2,111 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="flex justify-between items-center py-5 lg:px-32" >
-        <div>
+    <div >
+      <div className="navbar flex lg:flex-row justify-between lg:py-4 px-5 lg:px-32">
+        <div className="flex items-center">
           <img className="w-52" src="/public/image/Logo.png" alt="" />
         </div>
-        <div className="space-x-10">
-          <NavLink
-            to="/"
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-[#FF444A] underline font-bold"
-                : ""
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/Donation"
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-[#FF444A] underline font-bold"
-                : ""
-            }
-          >
-            Donation
-          </NavLink>
-          <NavLink
-            to="/Statistics"
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-[#FF444A] underline font-bold"
-                : ""
-            }
-          >
-            Statistics
-          </NavLink>
+        <div>
+          <div className="navbar-center flex-1 hidden lg:flex space-x-10">
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline font-bold"
+                  : ""
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/Donation"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline font-bold"
+                  : ""
+              }
+            >
+              Donation
+            </NavLink>
+            <NavLink
+              to="/Statistics"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline font-bold"
+                  : ""
+              }
+            >
+              Statistics
+            </NavLink>
+          </div>
+          <div>
+            <div className="dropdown -mr-4">
+              <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex="0"
+                className="menu dropdown-content mt-2 p-5 shadow bg-base-100 rounded-box relative -ml-28 w-40 space-y-2"
+              >
+                <NavLink
+                  to="/"
+                  className={({ isActive, isPending }) =>
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "text-[#FF444A] underline font-bold"
+                      : ""
+                  }
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/Donation"
+                  className={({ isActive, isPending }) =>
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "text-[#FF444A] underline font-bold"
+                      : ""
+                  }
+                >
+                  Donation
+                </NavLink>
+                <NavLink
+                  to="/Statistics"
+                  className={({ isActive, isPending }) =>
+                    isPending
+                      ? "pending"
+                      : isActive
+                      ? "text-[#FF444A] underline font-bold"
+                      : ""
+                  }
+                >
+                  Statistics
+                </NavLink>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
